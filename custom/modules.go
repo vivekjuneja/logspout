9453bfe2c934324ba6520f64add4d5fc2334b75b
Vivek Juneja <vivekjuneja@gmail.com>
2016-07-21 23:02:11 +0900

    logstash module added

--- custom/modules.go
+++ custom/modules.go
@@ -1,6 +1,8 @@
 package main
 
 import (
-	_ "github.com/gliderlabs/logspout/adapters/syslog"
-	_ "github.com/gliderlabs/logspout/transports/tcp"
+        _ "github.com/gliderlabs/logspout/adapters/syslog"
+        _ "github.com/gliderlabs/logspout/transports/tcp"
+        _ "github.com/vivekjuneja/logspout-logstash"
+        _ "github.com/gliderlabs/logspout/transports/udp"
 )
