2770189df64a44fc26e6e247a275ca67c0f48d6f
Vivek Juneja <vivekjuneja@gmail.com>
2016-08-17 05:30:51 +0000

    Replaced the github logspout-logstash plugin with gitlab version

--- custom/modules.go
+++ custom/modules.go
@@ -3,6 +3,6 @@ package main
 import (
         _ "github.com/gliderlabs/logspout/adapters/syslog"
         _ "github.com/gliderlabs/logspout/transports/tcp"
-        _ "github.com/vivekjuneja/logspout-logstash"
+        _ "gitlab.com/vivekjuneja/logspout-logstash"
         _ "github.com/gliderlabs/logspout/transports/udp"
 )
