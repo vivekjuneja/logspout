4bb62e71476eba76cf2814de54cf34dc0c835b7a
Vivek Juneja <vivekjuneja@gmail.com>
2016-09-07 05:43:15 +0000

    changed group from vivekjuneja to itsdp

--- custom/modules.go
+++ custom/modules.go
@@ -3,6 +3,6 @@ package main
 import (
         _ "github.com/gliderlabs/logspout/adapters/syslog"
         _ "github.com/gliderlabs/logspout/transports/tcp"
-        _ "gitlab.com/vivekjuneja/logspout-logstash"
+        _ "gitlab.com/itsdp/logspout-logstash"
         _ "github.com/gliderlabs/logspout/transports/udp"
 )
