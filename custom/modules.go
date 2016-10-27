d71e83221ad085f5c2bc3b6f70c188c9d1f8cf18
Vivek Juneja <vivekjuneja@gmail.com>
2016-10-27 18:42:03 +0900

    use github for logstash adapter

--- custom/modules.go
+++ custom/modules.go
@@ -4,5 +4,5 @@ import (
 	_ "github.com/gliderlabs/logspout/adapters/syslog"
 	_ "github.com/gliderlabs/logspout/transports/tcp"
 	_ "github.com/gliderlabs/logspout/transports/udp"
-	_ "gitlab.com/itsdp/logspout-logstash"
+	_ "github.com/vivekjuneja/logspout-logstash"
 )
